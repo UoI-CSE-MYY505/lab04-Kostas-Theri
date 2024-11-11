6a114f5b90e7d6aa1b2ce1e0ee184d01479d7f96
Kostas-Theri <cs05450@uoi.gr>
2024-11-11 17:51:05 +0200

    Completed lab04

--- lab04.s
+++ lab04.s
@@ -26,24 +26,40 @@ arrayNotSorted: .word marianna, markos, maria
             ecall
 
 str_ge:
-#---------
-# Write the subroutine code here
-#  You may move jr ra   if you wish.
-#---------
+            lbu  t0, 0(a0)
+            lbu  t1, 0(a1)
+            sub  t2, t0,   t1  #if equal result = 0 
+            addi a0, a0,   1
+            addi a1, a1,   1
+            add  t3, t1,   t0  #sum is equal to either, one must be 0
+            beq  t3, t0,   return_strcmp   #leave
+            beq  t2, zero, str_ge  #still equal, loop
+return_strcmp:
+            srli a0, t2, 31  #get the sign bit
+            xori a0, a0, 1 #Invert it
             jr   ra
- 
-# ----------------------------------------------------------------------------
-# recCheck(array, size)
-# if size == 0 or size == 1
-#     return 1
-# if str_ge(array[1], array[0])      # if first two items in ascending order,
-#     return recCheck(&(array[1]), size-1)  # check from 2nd element onwards
-# else
-#     return 0
 
 recCheck:
-#---------
-# Write the subroutine code here
-#  You may move jr ra   if you wish.
-#---------
+            slti t0, a1,   2
+            beq  t0, zero, checkFirstTwo
+            addi a0, zero, 1  #return 1
+            jr   ra
+checkFirstTwo:
+            addi sp, sp,   -12
+            sw   ra, 8(sp)
+            sw   a0, 4(sp)
+            sw   a1, 0(sp)
+            lw   a1, 0(a0)
+            lw   a0, 4(a0) 
+            jal  str_ge
+            beq  a0, zero, return  #return 0
+            lw   a0, 4(sp)    # get a0, a1
+            lw   a1, 0(sp)
+            addi a0, a0,   4   #check the rest
+            addi a1, a1,   -1  
+            jal  recCheck
+return:
+            lw   ra, 8(sp)
+            addi sp, sp,   12
+
             jr   ra
